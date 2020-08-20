2f48afebcbb66304181a7bfb87e72143ce754b98
Robert Nagoda <nagoda@gmail.com>
2020-08-19 21:26:18 -0700

    Added pytest diretory and tests

---
+++ pytest_tests/string_values.py
@@ -0,0 +1,15 @@
+class StringValues:
+
+    # Buttons
+    INCREMENT_BUTTON = 'button_001'
+    DECREMENT_BUTTON = 'button_002'
+    RESET_BUTTON = 'button_003'
+
+    # Text Fields
+    HEADER_TEXT_FIELD = 'textView_001'
+    DISPLAY_VALUE_TEXT_FIELD = 'textView_002'
+    WARNING_TEXT_FIELD = 'warningText'
+
+    # Warning Text Options
+    WARNING_TEXT_MAX_VALUE = 'WARNING: Maximum Value is 10'
+    WARNING_TEXT_MIN_VALUE = 'WARNING: Minimum Value is 0'
